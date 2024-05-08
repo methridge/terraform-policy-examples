4ec9299090e9573794600121beb22fc5c808dfd0
Michael Ethridge <michael.ethridge@cdw.com>
2024-05-08 12:29:15 -0600

    try no tag
    Signed-off-by: Michael Ethridge <michael.ethridge@cdw.com>

--- aws-instance/main.tf
+++ aws-instance/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
-  tags = {
-    Name = var.instance_name
-  }
+  # tags = {
+  #   Name = var.instance_name
+  # }
 }
